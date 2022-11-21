1b09e6fbd2d96c39dc7e44641d3fb292cc27acc1
leila008 <118557035+leila008@users.noreply.github.com>
2022-11-21 10:44:21 +0400

    Create mesele1.py
    Verilen edede qeder sade ededlerin sayini tapmaq

---
+++ mesele1.py
@@ -0,0 +1,9 @@
+eded=int(input("Neceye qederki asal ededleri tapmaq isteyirsiniz?:"))
+sade=[]
+for i in range(2,eded):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+         sade.append(i)
+print("0 ile {} arasinda {} dene sade eded var".format(eded,len(sade)))
